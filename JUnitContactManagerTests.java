853fc94cfb2054341d5865003d09049eea471deb
BBK-PiJ-2014-09 <dazza4968@yahoo.com>
2015-03-28 21:09:39 +0000

    JUNIT Test 1
    JUNIT Test1: test illegal argument (attempt to add a contact with a null
    name)

---
+++ JUnitContactManagerTests.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Class JUnitContactManagerTests - This is JUNIT test class for Contact Manager.
+ * 
+ * @author Daryl Smith, MSc IT 
+ * @version 1
+ */
+
+public class JUnitContactManagerTests
+{
+	@Test(expected = NullPointerException.class)
+	public void testAddNewContact1() 
+	{
+		//test illegal argument	(attempt to add a contact with a null name)
+		//test 1
+		ContactManagerImpl manager = new ContactManagerImpl();
+		manager.addNewContact(null, "VIP");
+	}
+
+}
