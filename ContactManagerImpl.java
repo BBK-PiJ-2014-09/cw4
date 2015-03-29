95df01fdcf2e03ae28212139af8dbcc2f277407c
BBK-PiJ-2014-09 <dazza4968@yahoo.com>
2015-03-29 15:52:54 +0100

    Add reader()method and implement flush()
    Add code to facilitate reading from a file reader() and persisting data
    by writing to a file flush().

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -346,9 +346,135 @@ public class ContactManagerImpl implements ContactManager {
 		return contactSet;
 	}
 
+	/** 
+	  * Method to read contact data from a file.
+	  * 
+	*/ 
+	public void reader() 
+	{
+		FileReader myFileReader;
+		try 
+		{
+			myFileReader = new FileReader("contacts.csv");
+			BufferedReader myReader = new BufferedReader(myFileReader);
+
+			String data = myReader.readLine();
+			
+			while (data != null) 
+			{
+				//System.out.println(data);
+				String[] myArray = data.split(",");
+				
+				//System.out.println(myArray[1]);
+				if (myArray[0].equals("CONTACT")) 
+				{
+					ContactImpl contact = new ContactImpl(Integer.parseInt(myArray[2]), myArray[1], myArray[3]);
+					myContacts.add(contact);
+				}
+				if (myArray[0].equals("PASTMEETING")) 
+				{
+					Calendar cal = new GregorianCalendar(Integer.parseInt(myArray[2]), 
+							Integer.parseInt(myArray[3]), 
+							Integer.parseInt(myArray[4]), 
+							Integer.parseInt(myArray[5]), 
+							Integer.parseInt(myArray[6]));
+					
+					int[] myContactArray = new int[myArray.length-8]; 
+					for (int i = 8; i< myArray.length; i++) 
+					{
+						myContactArray[i-8] = Integer.parseInt(myArray[i]); 
+					}
+					Set<Contact> myMeetingContacts = getContacts(myContactArray);
+
+					PastMeetingImpl pastMeeting = new PastMeetingImpl(Integer.parseInt(myArray[1]), cal, myMeetingContacts, myArray[7]);
+					
+					myMeetings.add(pastMeeting);
+				}
+					
+				if (myArray[0].equals("FUTUREMEETING")) 
+				{
+					Calendar cal = new GregorianCalendar(Integer.parseInt(myArray[2]), 
+							Integer.parseInt(myArray[3]), 
+							Integer.parseInt(myArray[4]), 
+							Integer.parseInt(myArray[5]), 
+							Integer.parseInt(myArray[6]));
+					
+					int[] myContactArray = new int[myArray.length-7]; 
+					for (int i = 7; i< myArray.length; i++) 
+					{
+						myContactArray[i-7] = Integer.parseInt(myArray[i]); 
+					}
+					Set<Contact> myMeetingContacts = getContacts(myContactArray);
+
+					FutureMeetingImpl futureMeeting = new FutureMeetingImpl(Integer.parseInt(myArray[1]), cal, myMeetingContacts);
+					
+					myMeetings.add(futureMeeting);
+				}
+					
+				data = myReader.readLine();
+			}
+		} catch (FileNotFoundException e) 
+		{
+			System.out.println("file not found");
+		} catch (IOException e) 
+		{
+			System.out.println("error reading file");			
+		}
+	}
+
 	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-		
-	}	
+	public void flush() 
+	{
+		FileWriter myWriter = null;
+		try 
+		{
+			myWriter = new FileWriter("contacts.csv");
+			
+			Iterator<Contact> myIterator = this.myContacts.iterator();
+			
+			while (myIterator.hasNext()) 
+			{
+				Contact tmpContact = myIterator.next();
+				myWriter.append("CONTACT," + tmpContact.getName() + "," + tmpContact.getId() + "," + tmpContact.getNotes() + "\n");
+			}
+
+			for(int i = 0; i < myMeetings.size(); i++) 
+			{
+				if (myMeetings.get(i) instanceof PastMeeting) 
+				{
+					myWriter.append("PASTMEETING," +  myMeetings.get(i).getId() + "," 
+					+ myMeetings.get(i).getDate().get(Calendar.YEAR) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.MONTH) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.DAY_OF_MONTH) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.HOUR) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.MINUTE) + ","	
+					+ ((PastMeeting)myMeetings.get(i)).getNotes());
+				}
+				if (myMeetings.get(i) instanceof FutureMeeting) 
+				{
+					myWriter.append("FUTUREMEETING," +  myMeetings.get(i).getId() + "," 
+					+ myMeetings.get(i).getDate().get(Calendar.YEAR) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.MONTH) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.DAY_OF_MONTH) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.HOUR) + ","
+					+ myMeetings.get(i).getDate().get(Calendar.MINUTE));
+				}
+				
+				Set<Contact> contactSet = myMeetings.get(i).getContacts();
+				Iterator<Contact> myContactIterator = contactSet.iterator();
+				
+				while (myContactIterator.hasNext()) 
+				{
+					Contact tmpContact = myContactIterator.next();
+					myWriter.append("," + tmpContact.getId());
+				}
+				myWriter.append("\n");				
+			}
+			myWriter.flush();
+			myWriter.close();		
+		} catch (IOException e) 
+		{
+			System.out.println("error writing to file");
+		}
+	}
 }
