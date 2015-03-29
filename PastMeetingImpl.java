904f8290a3888f5a3f7ec49119a4d4b81f8b2449
BBK-PiJ-2014-09 <dazza4968@yahoo.com>
2015-03-29 16:01:04 +0100

    Add code supporting Past Meetings
    Add code to facilitate past meetings

---
+++ PastMeetingImpl.java
@@ -0,0 +1,36 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Class PastMeetingImpl - extends the MeetingImpl class and implements the PastMeeting Interface.
+ * 
+ * @author Daryl Smith, MSc IT 
+ * @version 1
+ */
+
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting 
+{
+	
+	private String notes; 
+	
+	PastMeetingImpl(int Id, Calendar myCalendar, Set<Contact> mySet, String notes) 
+	{
+		super(Id, myCalendar, mySet);
+		this.notes = notes;
+	}
+
+	@Override
+	public String getNotes() 
+	{
+		if (this.notes == null)
+		{
+			return "";
+		}
+		return this.notes;
+	}
+
+	public void setNotes(String notes) 
+	{
+		this.notes = notes;
+	}
+}
